3157795cc009b31d120c93145e06636f0ee9c077
matvei <matvei@DESKTOP-3BIO0UB>
2021-10-12 18:08:47 +0300

    Добавьте файлы проекта.

---
+++ Learn CS/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Learn_CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Beep(); 
+        }
+    }
+}
